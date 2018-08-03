8a946805bf86569a0e84b78462b9b8937a0074f0
kgero <katystreet@gmail.com>
2018-08-03 17:10:38 -0400

    remove dashboards, add space btwn tweets

---
+++ main.js
@@ -0,0 +1,10 @@
+var dash = document.getElementsByClassName('dashboard');
+for(var i = 0; i < dash.length; i++) { 
+  dash[i].style.display='none'
+}
+
+var tweet = document.getElementsByClassName('tweet');
+for(var i = 0; i < tweet.length; i++) { 
+  tweet[i].style.marginBottom='200px';
+  tweet[i].style.marginTop='200px';
+}
