181833e0151873bc59967f858d0be4dc8cec8d3a
kgero <katystreet@gmail.com>
2018-08-03 22:32:33 -0400

    hide stream and recreate as i wish

--- main.js
+++ main.js
@@ -1,38 +1,41 @@
 
+var count = 0;
 
 var dash = document.getElementsByClassName('dashboard');
 for(var i = 0; i < dash.length; i++) { 
   dash[i].style.display='none'
 }
 
-var tweet = document.getElementsByClassName('tweet');
-for(var i = 1; i < tweet.length; i++) { 
-  tweet[i].style.display='none';
-  // tweet[i].style.marginBottom='200px';
-  // tweet[i].style.marginTop='200px';
-}
+// var tweet = document.getElementsByClassName('tweet');
+// for(var i = 1; i < tweet.length; i++) { 
+//   tweet[i].style.display='none';
+//   // tweet[i].style.marginBottom='200px';
+//   // tweet[i].style.marginTop='200px';
+// }
 
+// var timeline = document.getElementById('timeline');
+// timeline.style.display='none';
 
-var observer = new MutationObserver(function onMutationObserver(mutations) {
-  mutations.forEach(function(mutationNode) {
-    console.log("MUTATION!");
-    var mutationAddedNodes = mutationNode.addedNodes;
-    // No idea why the mutation API returns empty added mutations. Doesn't make
-    // sense, oh well ...
-    if (mutationAddedNodes && mutationAddedNodes.length > 0) {
-      // for some reason deleting the end loading spinner causes failure...
-      var end_loader = document.getElementsByClassName('stream-end-inner')[0];
-      
-      var tweet = document.getElementsByClassName('tweet');
-      for(var i = 1; i < tweet.length; i++) { 
-        tweet[i].style.display='none';
-        // tweet[i].style.marginBottom='200px';
-        // tweet[i].style.marginTop='200px';
-      }
-      var end_spinner = document.getElementsByClassName('spinner')[0];
-      end_spinner.classList.remove("spinner");
-    }
-  });
-});
-var historyContainerDOM = document.getElementById('timeline');
-observer.observe(historyContainerDOM, { childList: true, subtree: true });
+var stream = document.getElementsByClassName('stream')[0];
+stream.classList.add("oldstream");
+stream.style.display='none';
+console.log(stream);
+console.log(stream.children[0]);
+console.log(stream.children[0].children[0]);
+
+new_il = stream.children[0].children[0];
+console.log('new_il', new_il);
+var new_stream = document.createElement("div");
+new_stream.classList.add("stream");
+new_stream.classList.add("katyhere");
+var new_ol = document.createElement("ol");
+new_ol.classList.add("stream-items");
+new_ol.classList.add("js-navigable-stream");
+// this id is what twitter hooks into to load more tweets!
+// so don't add it back in!!
+// new_ol.id = "stream-items-id";
+new_ol.appendChild(new_il);
+new_stream.appendChild(new_ol);
+new_stream.style.display='contents';
+console.log(new_stream);
+stream.parentNode.insertBefore(new_stream, stream);
