7b736ba2988b485e3a941325bd519f1fe1fe48c0
kgero <katystreet@gmail.com>
2018-08-04 13:11:05 -0400

    one tweet at a time, continue button, increasing space

--- main.js
+++ main.js
@@ -1,5 +1,43 @@
-
 var count = 0;
+
+function add_tweet() {
+  var new_il = stream.children[0].children[count];
+  count++;
+  console.log('new_il', new_il);
+  var new_stream = document.createElement("div");
+  new_stream.classList.add("stream");
+  new_stream.classList.add("katyhere");
+  var new_ol = document.createElement("ol");
+  new_ol.classList.add("stream-items");
+  new_ol.classList.add("js-navigable-stream");
+  // this id is what twitter hooks into to load more tweets!
+  // so don't add it back in!!
+  // new_ol.id = "stream-items-id";
+  new_ol.appendChild(new_il);
+  new_stream.appendChild(new_ol);
+  new_stream.style.display='contents';
+  var white_space = document.createElement("div");
+  var pixels = 300 + count*50;
+  white_space.style.height=pixels.toString() + "px";
+  new_stream.appendChild(white_space);
+  console.log(new_stream);
+  var contin = document.getElementById("continue");
+  contin.parentNode.insertBefore(new_stream, contin);
+  // stream.parentNode.insertBefore(new_stream, stream);
+}
+
+function add_continue() {
+  var div = document.createElement("div");
+  var contin = document.createElement("p");
+  var text = document.createTextNode("continue?");
+  contin.appendChild(text);
+  console.log(contin);
+  contin.classList.add("lead");
+  contin.addEventListener('click', function() { add_tweet(); });
+  div.appendChild(contin);
+  div.id = "continue";
+  stream.parentNode.insertBefore(div, stream);
+}
 
 var dash = document.getElementsByClassName('dashboard');
 for(var i = 0; i < dash.length; i++) { 
@@ -19,23 +57,7 @@ for(var i = 0; i < dash.length; i++) {
 var stream = document.getElementsByClassName('stream')[0];
 stream.classList.add("oldstream");
 stream.style.display='none';
-console.log(stream);
-console.log(stream.children[0]);
-console.log(stream.children[0].children[0]);
 
-new_il = stream.children[0].children[0];
-console.log('new_il', new_il);
-var new_stream = document.createElement("div");
-new_stream.classList.add("stream");
-new_stream.classList.add("katyhere");
-var new_ol = document.createElement("ol");
-new_ol.classList.add("stream-items");
-new_ol.classList.add("js-navigable-stream");
-// this id is what twitter hooks into to load more tweets!
-// so don't add it back in!!
-// new_ol.id = "stream-items-id";
-new_ol.appendChild(new_il);
-new_stream.appendChild(new_ol);
-new_stream.style.display='contents';
-console.log(new_stream);
-stream.parentNode.insertBefore(new_stream, stream);
+add_continue();
+add_tweet();
+
